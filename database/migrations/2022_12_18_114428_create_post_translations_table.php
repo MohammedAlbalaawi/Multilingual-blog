5c324146a32626e8fac9eea2a7209a0ad4c70b4d
MohammedAlbalaawi <m.cip@live.com>
2022-12-18 14:02:30 +0200

    Install laravel translation package and create tables needed

---
+++ database/migrations/2022_12_18_114428_create_post_translations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_translations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->string('local')->unique();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->text('sub_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_translations');
+    }
+};
